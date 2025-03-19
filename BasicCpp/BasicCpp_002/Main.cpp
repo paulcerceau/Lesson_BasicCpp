a4786f860f885d5af571a1704731cad0b710c5c0
paulcerceau <oreyato.pro@gmail.com>
2025-03-19 17:51:30 +0100

    add: intro + variables

---
+++ BasicCpp/BasicCpp_002/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+using std::string;
+
+
+int main()
+{
+	string narratorName = "The Almighty";
+
+	cout << "Hello adventurer, I am " << narratorName << ".\nSounds cool right?" << endl;
+	cout << "What is your name? " << endl;
+	
+	string adventurerName;
+	cin >> adventurerName;
+
+	cout << "I am glad to meet you " << adventurerName << "!" << endl;
+
+	return 0;
+}
